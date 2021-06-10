4934e8599d44a577d2988e28da75c3e3301ed6ad
danascape <danascape@gmail.com>
2021-06-10 22:37:12 +0530

    sdm439-common: Use prebuilt dtbo
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,7 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm439
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
+BOARD_PREBUILT_DTBOIMAGE := device/xiaomi/sdm439-kernel/dtbo.img
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
