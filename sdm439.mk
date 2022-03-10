2119f7b472f4a58d492bec941c0372dd908ef007
Chuck439 <firmankediri123@gmail.com>
2022-03-10 18:05:01 +0000

    [dnm]sdm439-common: remove telephony-ext boot jars
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -396,9 +396,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
