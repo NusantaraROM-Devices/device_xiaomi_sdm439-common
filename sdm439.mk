5b24fa7d48ed7c7e048abbc574d4634721336983
Chuck439 <firmankediri123@gmail.com>
2022-03-14 22:48:32 +0000

    Revert "[SQUASH]sdm439-common: Drop oss cam hals for now"
    This reverts commit 4b57f76c55679a1c727976485e56a2e44e95eb77.

--- sdm439.mk
+++ sdm439.mk
@@ -113,16 +113,15 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    camera.msm8937 \
+    libmm-qcamera
 
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
 PRODUCT_PACKAGES += \
     GCamGOPrebuilt
-
-PRODUCT_PACKAGES += \
-    libwa_dof
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
